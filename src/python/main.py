da0fa7d83ec6a2edb446e239cf89221b0110afb5
juano2310 <juano23@gmail.com>
2017-01-03 16:22:17 -0500

    Main.py

--- src/python/main.py
+++ src/python/main.py
@@ -1,26 +1,44 @@
 import threading
 import time
-from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+from sense_hat import SenseHat
 from signal import pause
+import roboclaw
 import paho.mqtt.client as mqtt
+import subprocess
 
 server = "localhost"
 port = 1883
 vhost = "/"
 username = "guest"
 password = "guest"
+topic = "commands/#"
 
 sense = SenseHat()
 sense.set_imu_config(False, True, False)
 
-try:
-    	# set up mqtt client
-	client = mqtt.Client(client_id="", clean_session=True, userdata=None, protocol="MQTTv31")
-	client.username_pw_set(vhost + ":" + username, password)
-	client.connect(server, port, keepalive=60, bind_address="") #connect
-    	client.loop_start()
-except Exception, e:
-	print e
+#Windows comport name
+#roboclaw.Open("COM3",115200)
+#Linux comport name
+roboclaw.Open("/dev/ttyAMA0",115200)
+
+address = 0x80
+
+roboclaw.ForwardMixed(address, 0)
+roboclaw.TurnRightMixed(address, 0)
+
+def start_MQTT()
+    while True:
+        try:
+            client = mqtt.Client(client_id="", clean_session=True, userdata=None, protocol="MQTTv31")
+            client.username_pw_set(vhost + ":" + username, password)
+            client.on_connect = onConnect
+            client.on_message = onMessage
+            client.connect(server, port, keepalive=60, bind_address="") #connect
+            client.loop_forever()   #automatically reconnect once loop forever
+        except Exception, e:
+            #when initialize connection, reconnect on exception
+            print "Exception handled, reconnecting...\nDetail:\n%s" % e
+            time.sleep(5)
 
 def joystick_pushed(event):
     client.publish("commands/joystick", event.direction + "_" + event.action)
@@ -41,8 +59,40 @@ def sense_MQTT():
             client.publish("sense/roll", "{roll}".format(**accel_only))
             client.publish("sense/yaw", "{yaw}".format(**accel_only))
 
+def onConnect(client, userdata, rc):    #event on connecting
+    client.subscribe([(topic, 1)])  #subscribe
+    sense.show_message("Ready", text_colour=[255, 0, 255])
+    print("Ready")
+
+def onMessage(client, userdata, message):   #event on receiving message
+	roboAction = ""
+	if message.payload == "38":
+		roboclaw.ForwardMixed(address, 64)
+		roboAction = "Moving Forward"
+	elif message.payload == "40":
+		roboclaw.BackwardMixed(address, 64)
+		roboAction = "Moving Backward"
+	elif message.payload == "37":
+		roboclaw.TurnLeftMixed(address, 64)
+		roboAction = "Turning Left"
+	elif message.payload == "39":
+		roboclaw.TurnRightMixed(address, 64)
+		roboAction = "Turning Right"
+	elif message.payload == "middle_held":
+		subprocess.call(["node", "src/app.js", "--clear=true"])
+		roboAction = "Reset WiFi"
+	elif message.payload == "":
+		roboclaw.ForwardMixed(address, 0)
+		roboclaw.BackwardMixed(address, 0)
+		roboclaw.TurnRightMixed(address, 0)
+		roboclaw.TurnLeftMixed(address, 0)
+	if roboAction != "":	#Remove this IF to show all MQTT messages
+		print("Action: " + roboAction + ", Topic: " + message.topic + ", Message: " + message.payload)
+
+s = threading.Thread(target=start_MQTT)
 w = threading.Thread(target=joystick_MQTT)
 w2 = threading.Thread(target=sense_MQTT)
 
+s.start()
 w.start()
 w2.start()
