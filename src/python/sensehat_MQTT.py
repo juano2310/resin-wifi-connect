129d667388aa4f80392233486ac5136712a04500
juano2310 <juano23@gmail.com>
2017-01-03 14:52:56 -0500

    Add pressure

--- src/python/sensehat_MQTT.py
+++ src/python/sensehat_MQTT.py
@@ -21,8 +21,9 @@ try:
     while True:
             client.publish("sense/temp", round(sense.get_temperature(),1))
             client.publish("sense/humidity", round(sense.get_humidity(),0))
+            client.publish("sense/pressure", round(sense.get_pressure(),0))            
             accel_only = sense.get_accelerometer()
-            client.publish("sense/pitch", "{pitch}".format(**accel_only))   
+            client.publish("sense/pitch", "{pitch}".format(**accel_only))
             client.publish("sense/roll", "{roll}".format(**accel_only))
             client.publish("sense/yaw", "{yaw}".format(**accel_only))
             time.sleep(5)
