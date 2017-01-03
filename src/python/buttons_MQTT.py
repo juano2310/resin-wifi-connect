5797d32e8354e57d47ba35f307ad039e5edba43e
juano2310 <juano23@gmail.com>
2017-01-03 13:46:48 -0500

    Small fixes

--- src/python/buttons_MQTT.py
+++ src/python/buttons_MQTT.py
@@ -15,12 +15,13 @@ try:
 	client = mqtt.Client(client_id="", clean_session=True, userdata=None, protocol="MQTTv31")
 	client.username_pw_set(vhost + ":" + username, password)
 	client.connect(server, port, keepalive=60, bind_address="") #connect
-    	client.loop_forever()
+    	client.loop_start()
 except Exception, e:
     print e
+        client.loop_stop()
 
 def joystick_pushed(event):
-    client.publish("commands/joystick", event.direction + event.action)
+    client.publish("commands/joystick", event.direction + "_" + event.action)
 
 sense.stick.direction_any = joystick_pushed
 
